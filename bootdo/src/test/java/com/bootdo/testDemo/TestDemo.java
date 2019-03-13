669b1270a4c45323e814fb99f5dc8114f3368861
kt126 <1374507895@qq.com>
2019-03-13 11:11:09 +0800

    忽略文件增加applog

--- bootdo/src/test/java/com/bootdo/testDemo/TestDemo.java
+++ bootdo/src/test/java/com/bootdo/testDemo/TestDemo.java
@@ -8,7 +8,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestDemo {
