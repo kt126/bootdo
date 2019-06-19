f5efd33f9728c7fd82c726a7c0578bc930bda9c7
kt126 <1374507895@qq.com>
2019-06-19 16:56:47 +0800

    页面修改

--- bootdo/src/main/java/com/bootdo/system/domain/DeptDO.java
+++ bootdo/src/main/java/com/bootdo/system/domain/DeptDO.java
@@ -16,14 +16,14 @@ public class DeptDO implements Serializable {
 	
 	//
 	private Long deptId;
-	//上级部门ID，一级部门为0
-	private Long parentId;
-	//部门名称
-	private String name;
-	//排序
-	private Integer orderNum;
-	//是否删除  -1：已删除  0：正常
-	private Integer delFlag;
+    //上级部门ID，一级部门为0
+    private Long parentId;
+    //部门名称
+    private String name;
+    //排序
+    private Integer orderNum;
+    //是否删除  -1：已删除  0：正常
+    private Integer delFlag;
 
 	/**
 	 * 设置：
--- bootdo/src/test/java/com/bootdo/testDemo/TestDemo.java
+++ bootdo/src/test/java/com/bootdo/testDemo/TestDemo.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @RunWith(SpringRunner.class)
